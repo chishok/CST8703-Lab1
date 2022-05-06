d742422a09ec4793897b449b2f72713730c029f5
Kyle Chisholm <kyle.chisholm2@gmail.com>
2022-05-06 09:23:03 -0400

    update cyclictest script

--- scripts/run_cycletest.sh
+++ scripts/run_cycletest.sh
@@ -4,14 +4,17 @@ KERNEL_VERSION=$(uname -r)
 NOLOAD_FILE=./data/cyclictest_noload_${KERNEL_VERSION}.txt
 HIGHLOAD_FILE=./data/cyclictest_highload_${KERNEL_VERSION}.txt
 
+# make data directory
+mkdir -p ./data
+
 # run without cpu load
 sudo cyclictest -S -p 90 -m -l 100000 -q -h 500 --histfile="${NOLOAD_FILE}"
 
 # add load
-taskset -c 0 /bin/ping -l 60000 -q -s 10 -f localhost &
-taskset -c 1 /bin/ping -l 60000 -q -s 10 -f localhost &
-taskset -c 2 /bin/ping -l 60000 -q -s 10 -f localhost &
-taskset -c 3 /bin/ping -l 60000 -q -s 10 -f localhost &
+sudo taskset -c 0 /bin/ping -l 60000 -q -s 10 -f localhost &
+sudo taskset -c 1 /bin/ping -l 60000 -q -s 10 -f localhost &
+sudo taskset -c 2 /bin/ping -l 60000 -q -s 10 -f localhost &
+sudo taskset -c 3 /bin/ping -l 60000 -q -s 10 -f localhost &
 
 # run under load
 sudo cyclictest -S -p 90 -m -l 100000 -q -h 500 --histfile="${HIGHLOAD_FILE}"
